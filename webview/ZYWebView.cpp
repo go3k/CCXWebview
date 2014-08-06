caee48e131f2d3fbf2e9f56beb85b9c74308ef8c
go3k <omega.yue@gmail.com>
2014-08-06 17:30:51 +0800

    Add support for cocos2d-x v3.x

--- webview/ZYWebView.cpp
+++ webview/ZYWebView.cpp
@@ -7,7 +7,7 @@
 //
 
 #include "ZYWebView.h"
-#include "CCEGLView.h"
+//#include "CCGLView.h"
 
 ZYWebView::ZYWebView()
 {
@@ -21,17 +21,19 @@ bool ZYWebView::init()
 
 void ZYWebView::showWebView(const char* url, float x, float y, float width, float height)
 {
-    CCSize designsize = CCEGLView::sharedOpenGLView()->getDesignResolutionSize();
-    CCSize framesize = CCEGLView::sharedOpenGLView()->getFrameSize();
-    float sx = CCEGLView::sharedOpenGLView()->getScaleX();
-    float sy = CCEGLView::sharedOpenGLView()->getScaleY();
-    CCSize designframe(framesize.width / sx, framesize.height / sy);
+    auto glview = Director::getInstance()->getOpenGLView();
+    
+    Size designsize = glview->getDesignResolutionSize();
+    Size framesize = glview->getFrameSize();
+    float sx = glview->getScaleX();
+    float sy = glview->getScaleY();
+    Size designframe(framesize.width / sx, framesize.height / sy);
     
     // 这里可能需要根据ResolutionPolicy进行修改。
     // Modify this ratio equation depend on your ResolutionPolicy.
     float ratio = designsize.height / framesize.height;
     
-    CCPoint orig((designframe.width - designsize.width) / 2, (designframe.height - designsize.height) / 2);
+    Point orig((designframe.width - designsize.width) / 2, (designframe.height - designsize.height) / 2);
     
     x = x / ratio + orig.x / ratio; y = y / ratio + orig.y / ratio;
     width /= ratio; height /= ratio;
